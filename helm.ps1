3a510ee5a2cadbace028563c5c559f890687917f
Luc Conraads <59870716+LavaLaugh@users.noreply.github.com>
2024-06-22 22:13:58 +0200

    Update helm.ps1

--- helm.ps1
+++ helm.ps1
@@ -4,7 +4,7 @@ az login
 az account set --subscription 2b76758c-763b-41cb-ba92-6bc042e4710f
 az aks get-credentials --resource-group test --name aks101cluster
 
-helm install kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost --create-namespace --set kubecostToken="bHVjLmNvbnJhYWRzQHN0dWRlbnQuaHUubmw=xm343yadf98"
+helm install kubecost cost-analyzer --repo https://kubecost.github.io/cost-analyzer/ --namespace kubecost --create-namespace --set kubecostToken="..."
 kubectl port-forward --namespace kubecost deployment/kubecost-cost-analyzer 9090
 
 kubectl apply -f .\kubecost-lb.yaml
